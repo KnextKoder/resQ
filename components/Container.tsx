6b7c3f4b7c7a51ed60e51f2b70b772fab16a449b
knextkoder <knextkoderklan@gmail.com>
2026-02-09 14:04:41 +0100

    update icon file, delete uneeded files

--- components/Container.tsx
+++
@@ -1,6 +0,0 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return <SafeAreaView className='flex flex-1 m-6'>{children}</SafeAreaView>;
-};
-
